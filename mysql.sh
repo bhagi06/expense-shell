114a5893904448b7ccbb70213e6beef462c171ff
bhagi06 <kaparthibhargavi6@gmail.com>
2024-04-28 21:15:48 +0530

    save changes

--- mysql.sh
+++ mysql.sh
@@ -16,6 +16,15 @@ Y="\e[33m"
 N="\6[0m"
 
 #root access
+VALIDATE(){
+    if [$1 -ne 0 ]
+    then 
+    echo -e "$2....$R FAILURE $N"
+    exit 1
+    else
+    echo -e "$2-----$G sucess $N"
+    fi
+}
  if [ $USERID -ne 0 ]
 then
 echo " Please run this script with root access."
